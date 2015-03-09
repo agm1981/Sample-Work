b53998bfca5441aa92349cf16a5218f578d099ed
battarro@gmail.com <battarro@gmail.com>
2015-03-08 22:57:54 -0400

    second try

--- EnterpriseSolution/MembershipSystem/MembershipAppUser.cs
+++ EnterpriseSolution/MembershipSystem/MembershipAppUser.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using MembershipModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MembershipSystem
 {
-    public class MembershipApplicationUser : MembershipUser
+    public class MembershipAppUser : User, IUser<string>
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(MembershipUserManager<MembershipApplicationUser> manager, string authenticationType)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(MembershipUserManager<MembershipAppUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
--- EnterpriseSolution/MembershipSystem/MembershipUserManager.cs
+++ EnterpriseSolution/MembershipSystem/MembershipUserManager.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
-using MembershipModels;
 using Microsoft.AspNet.Identity;
 
 namespace MembershipSystem
 {
-    public class MembershipUserManager<TUser, TKey> : IDisposable
-        where TUser : class, IUser<TKey>
-        where TKey : System.IEquatable<TKey>
+    public partial class MembershipUserManager<TUser> : MembershipUserManager<TUser, string>
+        where TUser : class, Microsoft.AspNet.Identity.IUser<string>
     {
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-    }
 
-    public class MembershipUserManager<TUser> : MembershipUserManager<TUser, string> where TUser : class, IUser<string>
-    {
+
+        // Summary:
+        //     Constructor
+        //
+        // Parameters:
+        //   store:
         public MembershipUserManager(IUserStore<TUser> store)
         {
-        
+
         }
 
+        public MembershipUserManager(IUserStore<TUser> store, string v)
+        {
+        }
         public MembershipUserManager()
         {
-            ;
-        }
 
-
-        public Task<ClaimsIdentity> CreateIdentityAsync(MembershipApplicationUser membershipApplicationUser, string authenticationType)
-        {
-            throw new NotImplementedException();
         }
     }
+    public partial class MembershipUserManager<TUser, TKey>
+        where TUser : class, Microsoft.AspNet.Identity.IUser<TKey>
+        where TKey : System.IEquatable<TKey>
+    {
+
+    }
+
+
 }
